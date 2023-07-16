84c2c3d699500d50edf4579c5568513b31139cb5
valka-andrval <125165587+valka-andrval@users.noreply.github.com>
2023-07-17 00:05:44 +0300

    Build

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,6 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
